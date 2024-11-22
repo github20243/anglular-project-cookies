b059a206afa95932bc0e0b0b8c8111bade2ec6a2
nurs_996 <98372@gamil.com>
2024-11-22 15:03:41 +0600

    добавил tostify

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import {Component, HostListener} from '@angular/core';
-import {FormBuilder, Validators} from "@angular/forms";
-import {HttpClient} from "@angular/common/http";
+import { Component, HostListener } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
@@ -20,58 +21,61 @@ export class AppComponent {
     phone: ["", Validators.required],
   });
 
+
   mainImageStyle: any;
   orderImageStyle: any;
+
   @HostListener("document:mousemove", ["$event"])
   onMouseMove(e: MouseEvent) {
-    this.mainImageStyle = {transform: "translate(" + ((e.clientX * 0.3) / 8) + "px," + ((e.clientY * 0.3) / 8) + "px)"};
-    this.orderImageStyle = {transform: "translate(-" + ((e.clientX * 0.3) / 8) + "px,-" + ((e.clientY * 0.3) / 8) + "px)"};
+    this.mainImageStyle = { transform: "translate(" + ((e.clientX * 0.3) / 8) + "px," + ((e.clientY * 0.3) / 8) + "px)" };
+    this.orderImageStyle = { transform: "translate(-" + ((e.clientX * 0.3) / 8) + "px,-" + ((e.clientY * 0.3) / 8) + "px)" };
   }
 
-  constructor(private fb: FormBuilder,private http: HttpClient) {
-
-  }
+  constructor(
+    private fb: FormBuilder,
+    private http: HttpClient,
+    private toastr: ToastrService
+  ) { }
 
   ngOnInit() {
     setTimeout(() => {
       this.loaderShowed = false;
-    },2000);
+    }, 2000);
 
     setTimeout(() => {
       this.loader = false;
-    },3000);
+    }, 3000);
 
     this.http.get("https://testologia.ru/cookies").subscribe(data => this.productsData = data);
   }
 
   scrollTo(target: HTMLElement, product: any) {
-    target.scrollIntoView({behavior: 'smooth'});
+    target.scrollIntoView({ behavior: 'smooth' });
 
-    if(product) {
-      this.form.patchValue({product: product.title + ' (' + product.price + '' + this.currency + ')'});
+    if (product) {
+      this.form.patchValue({ product: product.title + ' (' + product.price + '' + this.currency + ')' });
     }
-
   }
 
-    switchSugarFree(e: any) {
-      this.http.get("https://testologia.ru/cookies" + (e.currentTarget.checked ? '?sugarfree' : ''))
-        .subscribe(data => this.productsData = data);
-    }
+  switchSugarFree(e: any) {
+    this.http.get("https://testologia.ru/cookies" + (e.currentTarget.checked ? '?sugarfree' : ''))
+      .subscribe(data => this.productsData = data);
+  }
 
   changeCurrency() {
-
     let newCurrency = "$";
     let coefficient = 1;
+
     if (this.currency === "$") {
       newCurrency = "₽";
       coefficient = 90;
-    }else if (this.currency === "₽") {
+    } else if (this.currency === "₽") {
       newCurrency = "BYN";
       coefficient = 3;
-    }else if (this.currency === "BYN") {
+    } else if (this.currency === "BYN") {
       newCurrency = "€";
       coefficient = 0.9;
-    }else if (this.currency === "€") {
+    } else if (this.currency === "€") {
       newCurrency = "¥";
       coefficient = 6.9;
     }
@@ -80,22 +84,26 @@ export class AppComponent {
 
     this.productsData.forEach((item: any) => {
       item.price = +(item.basePrice * coefficient).toFixed(1);
-    })
+    });
   }
 
   confirmOrder() {
-    if(this.form.valid) {
-      this.http.post("https://testologia.ru/cookies-order", this.form.value)
-      .subscribe( {
+    if (this.form.valid) {
+      this.http.post("https://testologia.ru/cookies-order", this.form.value).subscribe({
         next: (response: any) => {
-          alert(response.message);
+          this.toastr.success('Ваш заказ успешно отправлен!', 'Успех 🎉');
           this.form.reset();
         },
         error: (response: any) => {
-          alert(response.error.message);
+          this.toastr.error('Что-то пошло не так. Попробуйте еще раз.', 'Ошибка 😞');
         }
       });
+    } else {
+      this.toastr.warning('Пожалуйста, заполните все поля.', 'Внимание ⚠️');
     }
   }
 
+  showInfo() {
+    this.toastr.info('Это тестовое уведомление.', 'Информация ℹ️');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {ReactiveFormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {ToastrModule} from "ngx-toastr";
 
 @NgModule({
   declarations: [
     AppComponent
-  ],
-  imports: [
+  ],imports: [
     BrowserModule,
     ReactiveFormsModule,
     HttpClientModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      timeOut: 5000,
+      positionClass: 'toast-top-right',
+      preventDuplicates: true,
+      progressBar: true,
+      progressAnimation: 'increasing',
+      closeButton: true,
+    }),
   ],
+
   providers: [],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
